03f52dcf1f6de4b168c9b8af12659e1db10f3725
/Luis R Collymoore <lcollymoore@gmail.com>
2025-06-19 14:46:50 -0400

    update

--- Services/InputServices.cs
+++ Services/InputServices.cs
@@ -13,22 +13,43 @@ public class InputServices
     }
     public void InputData()
     {
-        Console.Write("Add Name: ");
-        string? name = Console.ReadLine();
-
-        Console.Write("Add Age: ");
-        int age = int.Parse(Console.ReadLine());
-
-        var newPerson = new Person
+        Console.Write("Select opt 1 for add person or 2 for exit loop: ");
+        string opt = Console.ReadLine();
+        if (opt == "1")
         {
-            Id = _people.Count + 1,
-            FirstName = name,
-            Age = age
 
-        };
-        Console.WriteLine($"New Argumen Added {newPerson.Id}, {newPerson.FirstName}");
+            while (true)
+            {
+                Console.Write("Add Name: ");
+                string? name = Console.ReadLine();
 
-        _people.Add(newPerson);
+                Console.Write("Add Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                var newPerson = new Person
+                {
+
+                    FirstName = name,
+                    Age = age
+
+                };
+                Console.WriteLine($"New Argumen Added {newPerson.Id}, {newPerson.FirstName}");
+
+                _people.Add(newPerson);
+                if (opt == "2")
+                {
+                    break;
+                }
+            }
+
+
+
+
+        }
+
+
+
+
     }
 
 }
